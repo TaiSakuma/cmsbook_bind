f832ddfc20fcb29c070ec5cc011c8c0d01f62b3a
Tai Sakuma <tai.sakuma@gmail.com>
2013-11-18 19:33:42 -0600

    remove the last separator

--- site_config/topcontents.php
+++ site_config/topcontents.php
@@ -4,7 +4,6 @@ $topcontents
 	array('head' => "Chapter A",     'parentDir' => 'chapter_A', 'href' => $cmsbook_root_url .'/chapter_A/test_001/web.php'),
 	array('separator' => True),
 	array('head' => "Chapter B",     'parentDir' => 'chapter_B', 'href' => $cmsbook_root_url .'/chapter_B/test_001/web.php'),
-	array('separator' => True),
 
 	array('right' => True),
 	array('head' => "Appendix",     'parentDir' => 'Appendix', 'href' => $cmsbook_root_url .'/Appendix/s0000_index_001'),
