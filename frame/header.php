59e9cc24c453d27867846b4e80615e28e173ce2e
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-11 20:49:33 +0200

    use elusive-icons

--- frame/header.php
+++ frame/header.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/screen.css"; ?>"  type="text/css" media="screen, projection" />
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/print.css"; ?>"  type="text/css" media="print" />
 <!--[if lt IE 8]><link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/ie.css"; ?>"  type="text/css" media="screen, projection"><![endif]-->
+<link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/elusive-icons-2.0.0/css/elusive-icons.min.css"; ?>" />
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/plugins/fancy-type/screen.css"; ?>"  type="text/css" media="screen, projection" />  
 <link rel="stylesheet" href="<?php echo $cmsbook_top_frame_url . "/layout.css"; ?>"  type="text/css" media="screen, projection" />
 <?php 
