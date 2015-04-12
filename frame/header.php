20fbc01ec7f50012de2cd7f68c233a7f2d44ef7b
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-12 15:23:46 +0200

    switch to html5

--- frame/header.php
+++ frame/header.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="UTF-8">
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/screen.css"; ?>"  type="text/css" media="screen, projection" />
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/print.css"; ?>"  type="text/css" media="print" />
 <!--[if lt IE 8]><link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/ie.css"; ?>"  type="text/css" media="screen, projection"><![endif]-->
