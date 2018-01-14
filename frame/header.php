76899887beea030b29be0baa0ebb888fdec34f24
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-14 11:23:08 +0100

    remove spaces at the end of line

--- frame/header.php
+++ frame/header.php
@@ -6,9 +6,9 @@
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/print.css"; ?>"  type="text/css" media="print" />
 <!--[if lt IE 8]><link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/ie.css"; ?>"  type="text/css" media="screen, projection"><![endif]-->
 <link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/elusive-icons-2.0.0/css/elusive-icons.min.css"; ?>" />
-<link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/plugins/fancy-type/screen.css"; ?>"  type="text/css" media="screen, projection" />  
+<link rel="stylesheet" href="<?php echo $cmsbook_external_url . "/blueprint/blueprint/plugins/fancy-type/screen.css"; ?>"  type="text/css" media="screen, projection" />
 <link rel="stylesheet" href="<?php echo $cmsbook_top_frame_url . "/layout.css"; ?>"  type="text/css" media="screen, projection" />
-<?php 
+<?php
  if ( is_file($cmsbook_site_config . "/site_html_head.php") )
    include($cmsbook_site_config . "/site_html_head.php");
 ?>
@@ -21,5 +21,5 @@
         if ( is_file($cmsbook_site_config . "/site_body_header.php") )
           include($cmsbook_site_config . "/site_body_header.php");
       ?>
-    </div><!--header--> 
+    </div><!--header-->
   </div>
