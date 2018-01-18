09703a907373d243fd56e533cc68622f3e754714
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-18 16:00:48 +0000

    add tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+require dirname(__DIR__) . '/vendor/autoload.php';
+include(dirname(__DIR__) . "/frame/pagemenu_functions.php");
---
+++ tests/mkSideNaviTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MkSideNaviTest extends TestCase
+{
+    public function testMkSideNavi()
+    {
+        mkSideNavi();
+    }
+}
